0ed2a623a39817e2e62a80f473dd1ca73a0ba7fc
gabytrejo <gabytrejo303@gmail.com>
2017-06-01 16:22:36 -0500

    Twitter: escritura y publicación de tweet con jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,24 @@
-
 var formulario = function(){
-	var toDo = document.getElementById("toDo");
-	toDo.addEventListener("submit", function(e){
-		e.preventDefault();
-		// Obtención de datos
-		var contenedor = document.getElementById("post");
-		var msjContenedor = document.getElementById("mensaje");
-		var msj = msjContenedor.value;
-		//Obtención de elementos
-		var postContenedor = document.createElement("article");
-		var post = document.createElement("p");
-		//Personalización de elementos
-		postContenedor.className = "jumbotron";
-		post.textContent = msj;
-		// Agregar al DOM
-		postContenedor.appendChild(post);
-		// Agregar a un elemento existente para visualizarlo
-		contenedor.appendChild(postContenedor);
-		// Borrar contenido de textarea
-		msjContenedor.value = "";
-
-	});
+	$("#tweet").submit(agregarTweet);
 };
 
-window.addEventListener("load", formulario);
+var agregarTweet = function(e){
+		e.preventDefault();
+		
+		//Obtención de elementos
+		var $postContenedor = $("<article />", { "class": "jumbotron" });
+		var $postTexto = $("<p></p>");
+		
+		//Personalización de elementos
+		$postTexto.text($("#mensaje").val());
+
+		// Agregar al DOM
+		$postContenedor.append($postTexto);
+		// Agregar a un elemento existente para visualizarlo
+		 $("#post").append($postContenedor);
+		// Borrar contenido de textarea
+		$("#mensaje").val("");
+
+};
+
+$(document).ready(formulario);
