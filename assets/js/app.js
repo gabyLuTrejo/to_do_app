6e0ec325d210be987c282ce68bb34cbf3bb0f8fc
Gaby Trejo <gabytrejo303@gmail.com>
2017-06-04 10:50:06 -0500

    Botón deshabilitado al enviar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,45 @@
 var formulario = function(){
 	$("#tweet").submit(agregarTweet);
+	$("#mensaje").keyup(validarTexto);
 };
 
 var agregarTweet = function(e){
-		e.preventDefault();
-		
-		//Obtención de elementos
-		var $postContenedor = $("<article />", { "class": "jumbotron" });
-		var $postTexto = $("<p></p>");
-		
-		//Personalización de elementos
-		$postTexto.text($("#mensaje").val());
+    e.preventDefault();
 
-		// Agregar al DOM
-		$postContenedor.append($postTexto);
-		// Agregar a un elemento existente para visualizarlo
-		 $("#post").append($postContenedor);
-		// Borrar contenido de textarea
-		$("#mensaje").val("");
+    //Obtención de elementos
+    var $postContenedor = $("<article />", { "class": "jumbotron" });
+    var $postTexto = $("<p></p>");
 
+    //Personalización de elementos
+    $postTexto.text($("#mensaje").val());
+
+    // Agregar al DOM
+    $postContenedor.append($postTexto);
+    // Agregar a un elemento existente para visualizarlo
+     $("#post").append($postContenedor);
+    // Borrar contenido de textarea
+    $("#mensaje").val("");
+    validarTexto();
+
+};
+
+var validarTexto = function(){
+    var $caracteres = conteoCaracteres();    
+	if($caracteres < 140){
+		$("#publicarTweet").removeAttr("disabled");
+				
+	} else {
+		$("#publicarTweet").attr("disabled", true);
+	}
+
+};
+
+var conteoCaracteres = function(){
+	var $caracteres = $("#caracteres").text(140-$("#mensaje").val().trim().length);
+    if($caracteres <= 0){
+        $("#caracteres").text("-" + $caracteres + $("#mensaje").val().trim().length);
+    }
+    return $caracteres.text();
 };
 
 $(document).ready(formulario);
